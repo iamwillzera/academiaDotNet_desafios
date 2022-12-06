4463ab0d99e4b618fee2e311b0929a6c700baf39
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-12-06 17:26:17 -0300

    Criadas as regras para ordenar as datas

---
+++ DesafioFinal/PlannerSemanal/Models/DataViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PlannerSemanal.Models
+{
+    public class DataViewModel
+    {
+     
+            public string Data { get; set; }
+            public string Identificadores { get; set; }
+    }
+}
