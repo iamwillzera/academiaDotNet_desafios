60742a41ab1f9d24249d477b95dc8e419531f14b
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-12-14 14:51:21 -0300

    Adicionando Readme

--- DesafioFinal/PlannerSemanal/Models/Atividade.cs
+++ DesafioFinal/PlannerSemanal/Models/Atividade.cs
@@ -13,7 +13,6 @@ namespace PlannerSemanal.Models
         [StringLength(50, ErrorMessage = "O nome da atividade deve conter, no máximo, 50 caracteres!")]
         public string Nome { get; set; }
 
-        [Required()]
         public string Data { get; set; }
 
         [Required(ErrorMessage = "{0} é um campo obrigatório!")]
